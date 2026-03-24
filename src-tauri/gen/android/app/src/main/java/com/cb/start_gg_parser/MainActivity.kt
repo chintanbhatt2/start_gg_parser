9c75572c1197898602951ebda23c33b230b63ef9
chintanbhatt2 <chintanbhatt2@gmail.com>
2026-03-24 15:32:22 -0700

    Initial Commit

---
+++ src-tauri/gen/android/app/src/main/java/com/cb/start_gg_parser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cb.start_gg_parser
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
